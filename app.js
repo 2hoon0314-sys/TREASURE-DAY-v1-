8b38a57460883e6293c2b7ce5752ebfcf5cf4916
2hoon0314-sys <2hoon0314@gmail.com>
2026-08-25 14:49:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -37607,61 +37607,7 @@ if (
 // 🃏 TREASURE CARD DATABASE
 // ========================================
 
-const treasureCardDatabase = [
 
- {
-  id: "PC-2026-VIVI-TWR-JH",
-
-  group: "TREASURE",
-
-  member: "JIHOON",
-
-  year: 2026,
-
-  source:
-    "ViVi 2026年5月号",
-
-  releaseDate:
-    "2026.03.23",
-
-  publisher:
-    "講談社",
-
-  category:
-    "MAGAZINE",
-
-  type:
-    "STORE BENEFIT",
-
-  store:
-    "TOWER RECORDS",
-
-  officialName:
-    "TREASURE ランダムセルフィートレカ",
-
-  card:
-    "Random Selfie Photocard",
-
-  set:
-    "1/10",
-
-  distribution:
-    "TOWER RECORDS",
-
-  note:
-    "ViVi 2026年5月号 タワーレコードオリジナル先着購入特典",
-
-  albumInclusion:
-    false,
-
-  popupBenefit:
-    false,
-
-  masterImage:
-    "cards/PC-2026-VIVI-TWR-JH.png"
-}
-
-];
 
 
 // 現在SCANで見つかったカード
